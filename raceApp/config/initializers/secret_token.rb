2f59798d92219899c3fd493c984876a63b3230b5
oprbguitar <oprbguitar@gmail.com>
2013-06-10 20:26:58 -0500

    creacion del proyecto
    se ha creado la base de caballos

---
+++ raceApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaceApp::Application.config.secret_token = 'f0532d09b2fe28913bb35d16092f6cf15a1ffcb83642bc3afdd3bb4718a6b60ace7c5d068b9f6042cfdf9f558398aa61188a13b92770a6007c70dd03ba364c9b'
